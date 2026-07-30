3a6a19c8270d3d2f0290ac023feafef2fb3bfacc
DevelSSN <shashankshantharamnayak@gmail.com>
2026-07-30 17:04:47 +0530

    feat(infra): enable pgvector extension in init-db.sh

--- infra/init-db.sh
+++ infra/init-db.sh
@@ -2,9 +2,11 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS vector;
     CREATE DATABASE ailms;
     CREATE USER ailms WITH PASSWORD '${AILMS_PASSWORD}';
     GRANT ALL PRIVILEGES ON DATABASE ailms TO ailms;
     \c ailms
+    CREATE EXTENSION IF NOT EXISTS vector;
     GRANT ALL ON SCHEMA public TO ailms;
 EOSQL
